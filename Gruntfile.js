fcb436c2278a14bfa3eed308e033222d8e9fc32d
ArtKalinin <akalinin.v@gmail.com>
2014-07-15 10:14:48 +0400

    IE 9,10,11

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,31 +11,67 @@ module.exports = function ( grunt ) {
                 user: "akalininv",
                 key: "899a7290-a8ec-4df9-9104-e32160e2042f",
                 //'tunnel-identifier': 'my-tunnel',
-                desiredCapabilities: 
-                {
-                    browserName: 'iehta',
-                    version: '9',
-                    platform: 'Windows 7',
-                    tags: ['IE','9'],
-                    name: process.env.TRAVIS_JOB_NUMBER ||'my test'
-                    //'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER || 'test'
+                updateSauceJob: true,
+                logLevel: 'verbose',
+                includeStackTrace: true,
+                
+                
+            },
+            iehta9: {
+                tests: ['tests/mainPage/links/*.js'],
+                options:{
+                    desiredCapabilities: {
+                        browserName: 'iehta',
+                        version: '9',
+                        platform: 'Windows 7',
+                        tags: ['IE','9'],
+                        name: 'Fame500, TRAVIS_JOB_NUMBER: ' + process.env.TRAVIS_JOB_NUMBER ||'Fame500 tests'
+                        //'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER || 'test'
+                    }
+                }
+                
+            },
+            iehta10: {
+                tests: ['tests/mainPage/links/*.js'],
+                options:{
+                    desiredCapabilities: {
+                        browserName: 'iehta',
+                        version: '10',
+                        platform: 'Windows 7',
+                        tags: ['IE','10','Windows 7'],
+                        name: 'Fame500, TRAVIS_JOB_NUMBER: ' + process.env.TRAVIS_JOB_NUMBER ||'Fame500 tests'
+                        //'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER || 'test'
+                    }
                 }
             },
-            chrome: {
-                tests: ['tests/mainPage/links/*.js']
+            iehta11: {
+                tests: ['tests/mainPage/links/*.js'],
+                options:{
+                    desiredCapabilities: {
+                        browserName: 'iehta',
+                        version: '11',
+                        platform: 'Windows 7',
+                        tags: ['IE','11','Windows 7'],
+                        name: 'Fame500, TRAVIS_JOB_NUMBER: ' + process.env.TRAVIS_JOB_NUMBER ||'Fame500 tests'
+                        //'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER || 'test'
+                    }
+                }
             }
         },
     };
 
     grunt.initConfig( grunt.util._.extend( taskConfig ) );
 
-    grunt.registerTask('menu links',  function () {
+    grunt.registerTask('iehta9',  function () {
         //execute some tests
-        grunt.task.run(['webdriver:chrome'], function() {
+        grunt.task.run(['webdriver:iehta9'], function() {
+            localserver.kill('SIGTERM');
+        });
+        grunt.task.run(['webdriver:iehta10'], function() {
             localserver.kill('SIGTERM');
         });
 
     });
 
-    grunt.registerTask('default', 'menu links');
+    grunt.registerTask('default', 'iehta9');
 };
